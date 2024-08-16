496f8c000af3f362f234f520644d361cc7d3f6a8
taehyun <jsun59590@gmail.com>
2024-08-16 14:26:54 +0900

    init_commit

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+user = 
+password = 
+ip = 
+db_name = 
+# MySQL 연결 설정
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{ip}/{db_name}"
+
+# SQLAlchemy 엔진 생성
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True  # 연결 확인을 위한 옵션
+)
+
+# 세션 로컬 설정
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 베이스 클래스 생성
+Base = declarative_base()
+
