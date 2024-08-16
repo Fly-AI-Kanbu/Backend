2f251a44adaccfbd11dcde185d4d284b3f52c845
jungch15 <58167392+jungch15@users.noreply.github.com>
2024-08-16 16:21:24 +0900

    Update models.py

--- models.py
+++ models.py
@@ -19,31 +19,40 @@ class User(Base):
     address = Column(String(60), nullable=False)
     country_id = Column(Integer, ForeignKey('country.country_id'), nullable=False)
 
+    korean_abilities = relationship("KoreanAbility", back_populates="user", cascade="all, delete-orphan")
     country = relationship("Country", back_populates="users")
+    access_tokens = relationship("AccessToken", back_populates="user", cascade="all, delete-orphan")
+    attendances = relationship("Attendance", back_populates="user", cascade="all, delete-orphan")
+    chats = relationship("Chat", back_populates="user", cascade="all, delete-orphan")
+    subscribe_logs = relationship("SubscribeLog", back_populates="user", cascade="all, delete-orphan")
+    log_quizzes = relationship("AnswerLog", back_populates="user", cascade="all, delete-orphan")
+
+
 
 class KoreanAbility(Base):
     __tablename__ = 'korean_ability'
 
     ability_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), primary_key=True, default=1)
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False)
     complexity = Column(Integer, nullable=False)
     toxicity = Column(Integer, nullable=False)
     fluency = Column(Integer, nullable=False)
     vocabulary = Column(Integer, nullable=False)
     similarity = Column(Integer, nullable=False)
 
-    user = relationship("User")
+    user = relationship("User", back_populates="korean_abilities")
 
 class Chat(Base):
     __tablename__ = 'chat'
 
     chat_id = Column(String(50), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False, default=1)
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False, default=1)
     subject_id = Column(Integer, ForeignKey('subject.subject_id'), nullable=False)
     created_time = Column(DateTime, nullable=False)
 
     user = relationship("User")
     subject = relationship("Subject")
+    messages = relationship("ChatMessage", back_populates="chat", cascade="all, delete-orphan")
 
 class VocaPair(Base):
     __tablename__ = 'voca_pair'
@@ -56,24 +65,22 @@ class ChatMessage(Base):
     __tablename__ = 'chat_message'
 
     msg_id = Column(String(50), primary_key=True)
-    chat_id = Column(String(50), ForeignKey('chat.chat_id'), primary_key=True)
+    chat_id = Column(String(50), ForeignKey('chat.chat_id', ondelete="CASCADE"), nullable=False)
     content = Column(Text, nullable=False)
     created_time = Column(DateTime, nullable=False)
     is_human = Column(Boolean, nullable=False)
 
-    chat = relationship("Chat")
-
+    chat = relationship("Chat", back_populates="messages")
 class AnswerLog(Base):
-    __tablename__ = 'answer_log'
+    __tablename__ = 'log_quiz'
 
     log_id = Column(String(10), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
-    word_id = Column(Integer, ForeignKey('voca_pair.voca_id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False)
+    word_id = Column(Integer, nullable=False)
     is_answer = Column(Boolean, nullable=False)
     date = Column(DateTime, nullable=False)
 
-    user = relationship("User")
-    voca_pair = relationship("VocaPair")
+    user = relationship("User", back_populates="log_quizzes")
 
 class Subject(Base):
     __tablename__ = 'subject'
@@ -86,17 +93,15 @@ class Attendance(Base):
     __tablename__ = 'attendance'
 
     attendance_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), primary_key=True)
-    attendance_date = Column(Date, nullable=False)
-
-    user = relationship("User")
-
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False)
+    attendance_date = Column(DateTime, nullable=False)
+    user = relationship("User", back_populates="attendances")
 class Country(Base):
     __tablename__ = 'country'
 
     country_id = Column(Integer, primary_key=True, autoincrement=True)
     iso = Column(CHAR(2), nullable=True)
-    name = Column(String(30), nullable=True)
+    name = Column(String(20), nullable=True)
     nicename = Column(String(20), nullable=True)
     iso3 = Column(CHAR(3), nullable=True)
     numcode = Column(SmallInteger, nullable=True)
@@ -114,12 +119,12 @@ class Subscribe(Base):
 class AccessToken(Base):
     __tablename__ = 'access_token'
 
-    Key = Column(BigInteger, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    key = Column(BigInteger, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"), nullable=False)
     access_token = Column(Text, nullable=False)
     expiration_date = Column(DateTime, nullable=False)
 
-    user = relationship("User")
+    user = relationship("User", back_populates="access_tokens")
 
 class SubscribeLog(Base):
     __tablename__ = 'subscribe_log'
